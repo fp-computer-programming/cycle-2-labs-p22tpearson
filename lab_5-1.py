e636c30b4eaee618585101f6a5d9a18536c1ce1b
p22tpearson <p22tpearson@fairfieldprep.org>
2021-09-20 10:04:36 -0400

    add file

---
+++ lab_5-1.py
@@ -0,0 +1,34 @@
+# author: cjp (amdg) 9/20/2021
+
+#question 1
+# x+=5
+x=x+5
+
+# question 2
+# y=y-3
+y -=3
+
+#question 3
+#z **=2
+z=z **= 2
+z = z **2
+
+# question 4
+#w =w % 2
+w %= 2
+
+#question 5
+# t = t / 5
+t /=5
+
+# question 6
+# a *=12
+a = a * 12
+
+# question 7
+# i = i // 7
+i //= 7
+
+a += a + 2
+
+a = 2 * a + 2
